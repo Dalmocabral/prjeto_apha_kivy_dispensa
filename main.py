657d9dafb105a54b80ce80d700d3adcebfe5d628
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2024-06-07 14:43:58 -0300

    commit

--- main.py
+++ main.py
@@ -1,76 +1,74 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.checkbox import CheckBox
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.checkbox import CheckBox
 from kivy.core.clipboard import Clipboard
 from datetime import datetime
-from models import session, User
+from models import User, session
 
-class UserList(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'vertical'
+class MenuScreen(Screen):
+    pass
+
+class AddUserScreen(Screen):
+    pass
+
+class UserListScreen(Screen):
+    def on_pre_enter(self, *args):
         self.refresh_users()
 
     def refresh_users(self):
-        self.clear_widgets()
+        self.ids.users_box.clear_widgets()
         users = session.query(User).order_by(
             User.afastado.asc(),
             User.dataDispensa.asc().nullsfirst()
         ).all()
 
-        for user in users:
+        for idx, user in enumerate(users, start=1):
             user_box = BoxLayout(size_hint_y=None, height=50, spacing=10)
-            user_box.add_widget(Label(text=str(user.id), size_hint_x=0.1))
+            user_box.add_widget(Label(text=str(idx), size_hint_x=0.1))
             user_box.add_widget(Label(text=user.name, size_hint_x=0.3))
             
             afastado_checkbox = CheckBox(size_hint_x=0.1)
             afastado_checkbox.active = user.afastado
-            afastado_checkbox.bind(on_release=lambda chk, user=user: self.toggle_afastado(user))
+            afastado_checkbox.bind(active=lambda checkbox, value, u=user: self.set_afastado(u, value))
             user_box.add_widget(afastado_checkbox)
 
             dispensa_button = Button(text='Dispensa', size_hint_x=0.2)
-            dispensa_button.bind(on_press=lambda btn, user=user: self.dispensa(user))
+            dispensa_button.bind(on_press=lambda btn, u=user: self.dispensa_user(u))
             user_box.add_widget(dispensa_button)
 
-            self.add_widget(user_box)
+            self.ids.users_box.add_widget(user_box)
 
-    def toggle_afastado(self, user):
-        user.afastado = not user.afastado
+    def set_afastado(self, user, value):
+        user.afastado = value
         session.commit()
         self.refresh_users()
 
-    def dispensa(self, user):
+    def dispensa_user(self, user):
         user.dataDispensa = datetime.utcnow()
         session.commit()
         self.refresh_users()
 
 class DispensaApp(App):
     def build(self):
-        main_layout = BoxLayout(orientation='vertical')
-        self.user_input = TextInput(hint_text='Nome do Usuário', size_hint_y=None, height=50)
-        add_user_button = Button(text='Adicionar Usuário', size_hint_y=None, height=50)
-        add_user_button.bind(on_press=lambda _: self.add_user(self.user_input.text))
-        main_layout.add_widget(self.user_input)
-        main_layout.add_widget(add_user_button)
-        main_layout.add_widget(UserList())
-        copy_btn = Button(text='Copiar para WhatsApp', size_hint_y=None, height=50)
-        copy_btn.bind(on_press=self.copy_to_clipboard)
-        main_layout.add_widget(copy_btn)
-        return main_layout
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(AddUserScreen(name='add_user'))
+        sm.add_widget(UserListScreen(name='user_list'))
+        return sm
 
     def add_user(self, user_name):
         if user_name.strip():
             new_user = User(name=user_name)
             session.add(new_user)
             session.commit()
-            self.root.children[1].refresh_users()
-            self.user_input.text = ''
+            self.root.get_screen('user_list').refresh_users()
 
-    def copy_to_clipboard(self, instance=None):
+    def copy_to_clipboard(self):
         users = session.query(User).filter_by(afastado=False).all()
         date_str = datetime.now().strftime('%d/%m/%Y')
         text = f'*DISPENSA ATUALIZADA* {date_str}\n\n'
